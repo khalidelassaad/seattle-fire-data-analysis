4f0c0155d07721311523c0f3bf7881a056306967
Khalid Elassaad <khalidelassaad@gmail.com>
2026-02-09 16:40:45 -0800

    code: proof of concept - fetching data

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from dotenv import load_dotenv
+from sodapy import Socrata
+
 import os 
+import pandas as pd
 
 load_dotenv() 
 
 if __name__ == "__main__":
-    print(os.environ['SOCRATA_API_KEY_ID'])
-    print(os.environ['SOCRATA_API_KEY_SECRET'])
+    APP_TOKEN = os.environ['SOCRATA_APP_TOKEN']
+    APP_SECRET = os.environ['SOCRATA_APP_SECRET']
+
+    client = Socrata("data.seattle.gov", APP_TOKEN)
+    result = client.get("kzjm-xkqj")
+
+    print(result)
