f8356deb2bf448eea885558e10a068d3e153104b
Khalid Elassaad <khalidelassaad@gmail.com>
2026-02-13 18:20:55 -0800

    code: read_csv with python engine

--- sfd_unit_analysis.py
+++ sfd_unit_analysis.py
@@ -18,17 +18,20 @@ def _(mo, pd):
     incidents_dataframe = pd.read_csv(
         mo.notebook_location() / "public" / "incidents_last_30_days.csv", 
         index_col=0,
-        compression=None
+        compression=None,
+        engine="python"
     )
     unit_dispatches_dataframe = pd.read_csv(
         mo.notebook_location() / "public" / "unit_dispatches_last_30_days.csv", 
         index_col=0,
-        compression=None
+        compression=None,
+        engine="python"
     )
     unit_dataframe = pd.read_csv(
         mo.notebook_location() / "public" / "unit_stats_last_30_days.csv", 
         index_col=0,
-        compression=None
+        compression=None,
+        engine="python"
     )
     return incidents_dataframe, unit_dataframe
 
