3f01437b9d2694a9efc37131f367956b0dc99730
Khalid Elassaad <khalidelassaad@gmail.com>
2026-02-18 11:32:37 -0800

    code: test altair visualization

--- sfd_unit_analysis.py
+++ sfd_unit_analysis.py
@@ -172,6 +172,24 @@ def _(mo, stat_list, unit_dropdown):
 
 @app.cell
 def _():
+    # import altair with an abbreviated alias
+    import altair as alt
+
+    # load a sample dataset as a pandas DataFrame
+    from altair.datasets import data
+    cars = data.cars()
+
+    # make the chart
+    alt.Chart(cars).mark_point().encode(
+        x='Horsepower',
+        y='Miles_per_Gallon',
+        color='Origin',
+    ).interactive()
+    return
+
+
+@app.cell
+def _():
     return
 
 
